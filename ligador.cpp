dc24177a5698df55e69c9638c1c756760ab7f0a6
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-10-11 16:50:07 -0300

    first commit

---
+++ ligador.cpp
@@ -0,0 +1,14 @@
+// TRABALHO 1 DE SOFTWARE BÁSICO - UNB - 2/2018
+// PROFESSOR - BRUNO
+// ALUNOS:
+// - FREDERICO PINHEIRO DIB - 15/0125925
+// - LUCAS DA SILVA MOUTINHO - 15/0015747
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "FALA FRED" << endl;
+  return 0;
+}
