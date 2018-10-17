2353348ddc4278046856782ad762f52bed73b6a9
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-10-17 16:34:21 -0300

    arquivo abrindo com sucesso

--- montador.cpp
+++ montador.cpp
@@ -5,10 +5,56 @@
 // - LUCAS DA SILVA MOUTINHO - 15/0015747
 
 #include <iostream>
-
+#include <string>
+#include <algorithm>
+#include <fstream>
 using namespace std;
 
-int main() {
+void first_passage() {
   cout << "FALA FRED" << endl;
+  string s;
+  getline(cin, s);
+  transform(s.begin(), s.end(), s.begin(), ::toupper); /*Deixa toda a string maiuscula*/
+  cout << s << endl;
+}
+
+void second_passage() {
+}
+
+bool correcet_execution(int argc, string s) {
+  bool correct=true;
+  if (argc != 2) {
+    cout << "Numero de argumentos errados!" << endl;
+    correct = false;
+  }
+  if (s.length() <= 4) {
+    cout << "Arquivo fora de formato!" << endl;
+    correct = false;
+  }
+  else if (s.find(".asm") == string::npos) {
+    cout << "Arquivo fora de formato!" << endl;
+    correct = false;
+  }
+  return correct;
+}
+
+bool fexists(const string &filename){
+  ifstream ifile(filename.c_str());
+  return (bool) ifile;
+}
+
+int main(int argc, char *argv[]) {
+  ifstream inputfile;
+  if (correcet_execution(argc, argv[1])) {
+    if (fexists(argv[1])) {
+      inputfile.open(argv[1]);
+      first_passage();
+      second_passage();
+    }
+    else{
+      cout << "O Arquivo "<< argv[1] <<" não existe" << endl;
+    }
+  }
+
   return 0;
 }
---
+++ test.asm
@@ -0,0 +1,15 @@
+SECTION TEXT
+ROT:    INPUT       N1
+      COPY N1, N4 ;comentario qualquer
+ROT2:
+      COPY N2, N3
+COPY N3, N3 + 1
+OUTPUT N3 + 1
+STOP
+;comentario qualuer
+SECTIO DATA
+N2: CONST -5
+SECTION BSS
+N1: SPACE
+N3: SPACE 2
+N4: SPACE
