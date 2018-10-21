5e49a278f828344c7f0b73fe63004411ec6bfe59
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-10-21 19:29:21 -0300

    logica de definitions table e use table para primeira passagem

--- montador.cpp
+++ montador.cpp
@@ -16,6 +16,17 @@ using namespace std;
 struct symbols {
   string label;
   int position_count;
+  int extern_value; // 0 se não é valor externo e 1 se é
+};
+
+struct definition {
+  string label;
+  int value;
+};
+
+struct use {
+  string label;
+  int position_count;
 };
 
 struct instruction_length{
@@ -43,6 +54,8 @@ struct const_struct {
 };
 
 vector<symbols> symbols_table; /*Tabela de simbolos*/
+vector<definition> definitions_table; /*Tabela de Definições*/
+vector<use> use_table; /*Tabela de Uso*/
 vector<instruction_length> instructions_table; /*Tabela de instruções*/
 vector<directive_length> directives_table; /*Tabela de diretivas*/
 vector<section_length> section_table; /*Tabela das sessões*/
@@ -435,6 +448,19 @@ void print_TS(){
   cout << "-------------" << endl;
 }
 
+/* Print Definitions Table no terminal */
+void print_deftable(){
+  unsigned int table_size = (unsigned int)definitions_table.size();
+  unsigned int i;
+
+  cout << "-------------" << endl;
+  for(i = 0; i < table_size; i++){
+    cout << definitions_table[i].label << endl;
+    cout << definitions_table[i].value << endl << endl;
+  }
+  cout << "-------------" << endl;
+}
+
 void print_section_table() {
   unsigned int table_size = (unsigned int)section_table.size();
   unsigned int i;
@@ -489,7 +515,7 @@ void print_code() {
 
 // Procura rótulo na Tabela de Símbolos. Se achou emite um erro de símbolo redefinido e retorna -1
 // Senão insere rótulo e position_count na TS e retorna 0
-int insert_label_TS(string label, int position_count){
+int insert_label_TS(string label, int position_count, bool extern_value = false){
   unsigned int table_size = (unsigned int)symbols_table.size();
   unsigned int i = 0;
   bool found = false;
@@ -508,9 +534,15 @@ int insert_label_TS(string label, int position_count){
   }
 
   symbols new_symbol;
-  new_symbol.label = label; 
-  new_symbol.position_count = position_count;
-
+  new_symbol.label = label;
+  if(!extern_value){
+    new_symbol.extern_value = 1;
+    new_symbol.position_count = 0;
+  }
+  else{
+    new_symbol.extern_value = 0;
+    new_symbol.position_count = position_count;
+  }
   symbols_table.push_back(new_symbol); 
 
   return 0;
@@ -567,6 +599,17 @@ void insert_const_table(vector<string> words, int position_count) {
   }
 }
 
+void insert_definitions_table(vector<string> words) {
+  definition new_definition;
+
+  if (words.size() == 3) {
+    if (words[1] == "PUBLIC") {
+      new_definition.label = words[2];
+      definitions_table.push_back(new_definition);
+    }
+  }
+}
+
 void verify_text_section() {
   unsigned int table_size = (unsigned int)section_table.size();
   unsigned int i, flag=0;
@@ -611,6 +654,15 @@ void insert_relative(vector<string> words, int line_count) {
 
 }
 
+void update_definitions_table(){
+  unsigned int i;
+  unsigned int table_size = (unsigned int)definitions_table.size();
+
+  for(i = 0; i < table_size; i++){
+    definitions_table[i].value = get_symbol_value(definitions_table[i].label);
+  }
+}
+
 /*Função de primeira passagem do montador*/
 void first_passage(char *argv[]) {
   ifstream inputfile;
@@ -648,7 +700,12 @@ void first_passage(char *argv[]) {
       insert_relative(words, line_count);
 
       if(words[0] != "none"){ //Rótulo existe
-        error = insert_label_TS(words[0],position_count);
+        if(words[1] == "EXTERN"){
+          error = insert_label_TS(words[0],position_count, true);
+        }
+        else{
+          error = insert_label_TS(words[0], position_count);
+        }
         if(error == -1){
           cout << "ERROR: Simbolo redefinido na linha: " << line_count << endl;
           exit(0);
@@ -670,6 +727,7 @@ void first_passage(char *argv[]) {
           // executa diretiva
           insert_section_table(words, position_count, line_count);
           insert_const_table(words, position_count);
+          insert_definitions_table(words);
           position_count = position_count + length;
         }
         else{
@@ -685,6 +743,7 @@ void first_passage(char *argv[]) {
     }
   }
   verify_text_section();
+  update_definitions_table();
 }
 
 /*Verifica os erros de operandos*/
@@ -1048,6 +1107,7 @@ int main(int argc, char *argv[]) {
         initialize_instructions_table();
         first_passage(argv);
         second_passage(argv);
+        print_deftable();
       }
       else{
         cout << "ERROR: O Arquivo "<< argv[1] <<" não existe" << endl;
